a09ea974601c41fc1809e997b2d2c1a23d169ab5
Banktts <bankthamasaroch@gmail.com>
2024-07-23 15:16:01 +0700

    update ip

--- src/main/java/com/example/demo_log/service/HelloWorldService.java
+++ src/main/java/com/example/demo_log/service/HelloWorldService.java
@@ -6,15 +6,31 @@ import lombok.experimental.FieldDefaults;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
 @Slf4j
 @Service
 @RequiredArgsConstructor
 @FieldDefaults(makeFinal = true, level = AccessLevel.PRIVATE)
 public class HelloWorldService {
 
+
     public String inquiryHelloWorld(){
         log.error("hello");
-        return "hello world";
+        String ipAddress = getServerIpAddress();
+        return "hello world from " + " with IP: " + ipAddress;
+    }
+
+
+    private String getServerIpAddress() {
+        try {
+            InetAddress inetAddress = InetAddress.getLocalHost();
+            return inetAddress.getHostAddress();
+        } catch (UnknownHostException e) {
+            log.error("Unable to get server IP address", e);
+            return "unknown";
+        }
     }
 
 }
